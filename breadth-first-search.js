7c669b26df26b5630c4edd89200b34ffbfc3b58a
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-06-08 09:10:57 +0200

    adding bfst

---
+++ breadth-first-search.js
@@ -0,0 +1,77 @@
+const breadthFirstTraverse2 = (queue, array) => {
+  // recursive
+  if (!queue.length) return array;
+
+  const node = queue.shift();
+  array.push(node.value);
+  if (node.left) queue.push(node.left);
+  if (node.right) queue.push(node.right);
+  return breadthFirstTraverse2(queue, array);
+};
+
+const breadthFirstTraverse = (queue, array) => {
+  // iterative
+  while (queue.length) {
+    const node = queue.shift()
+    array.push(node.value);
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+  }
+  return array;
+}
+
+// unit tests
+// do not modify the below code
+describe.skip("breadth-first tree traversal", function () {
+  const answer = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+
+  const tree = {
+    value: "A",
+    left: {
+      value: "B",
+      left: {
+        value: "D",
+        left: {
+          value: "G",
+          left: null,
+          right: null
+        },
+        right: null
+      },
+      right: {
+        value: "E",
+        left: null,
+        right: {
+          value: "H",
+          left: {
+            value: "K",
+            left: null,
+            right: null
+          }
+        }
+      }
+    },
+    right: {
+      value: "C",
+      left: {
+        value: "F",
+        left: {
+          value: "I",
+          left: null,
+          right: null
+        },
+        right: {
+          value: "J",
+          left: null,
+          right: null
+        }
+      },
+      right: null
+    }
+  };
+
+  test("breadthFirstTraverse", () => {
+    expect(breadthFirstTraverse([tree], [])).toEqual(answer);
+  });
+});
+
