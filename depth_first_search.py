10c88c6391a638366568e0aabcb8ce557e6802e6
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-17 19:03:59 +0200

    BFS bst's workings

---
+++ depth_first_search.py
@@ -0,0 +1,23 @@
+"""
+BST -> Depth-First-Search (DFS)
+"""
+def inorder(root):
+    if not root:
+        return
+    inorder(root.left)
+    print(root.val)
+    inorder(root.right)
+
+def preorder(root):
+    if not root:
+        return
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+def postorder(root):
+    if not root:
+        return
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
