58602bce0636297512df2b1b4546766d9267f967
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-19 19:46:23 +0200

    super basic hash map implementation

---
+++ basic_hashmap.py
@@ -0,0 +1,14 @@
+"""
+Basic hasmap implementation
+Time complexity: 0(1) --> average case
+"""
+
+names = ["alice", "brad", "collin", "brad", "dylan", "kim"]
+
+countMap = {}
+for name in names:
+    # If countMap does not contain name
+    if name not in countMap:
+        countMap[name] = 1
+    else:
+        countMap[name] += 1
