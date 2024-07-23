3648c4e6fcd7ffc26dc6f4acdb7597a77a0d8301
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-23 19:52:27 +0200

    graphs - adj list impl 1

---
+++ graph_adjacency_list.py
@@ -0,0 +1,24 @@
+"""
+Graphs --> Adjacency List
+"""
+
+# GraphNode used for adjacency list
+class GraphNode:
+    def __init__(self, val):
+        self.val = val
+        self.neighbours = []
+
+# Or use a HashMap
+adjList = { "A": [], "B": [] }
+
+# Given directed edges, build an adjacency list
+edges = [["A", "B"], ["B", "C"], ["B", "E"], ["C ", "E"], ["E", "D"]]
+
+adjList = {}
+
+for src, dst in edges:
+    if src not in adjList:
+        adjList[src] = []
+    if dst not in adjList:
+        adjList[dst] = []
+    adjList[src].append(dst)
