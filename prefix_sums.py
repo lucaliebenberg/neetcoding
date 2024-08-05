5a65f43f8ac5613065b0d9d673d82256af8bcff8
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-08-05 20:27:42 +0200

    working with prefix sums

---
+++ prefix_sums.py
@@ -0,0 +1,19 @@
+"""
+Prefix Sums
+"""
+#       2   1  4   1  5
+nums = [2, -1, 3, -3, 4]
+
+class PrefixSum:
+
+    def __init__(self, nums):
+        self.prefix = []
+        total = 0
+        for n in nums:
+            total += n
+            self.prefix.append(total)
+    
+    def rangeSum(self, left, right):
+        preRight = self.prefix[right]
+        preLeft = self.prefix[left - 1] if left > 0 else 0
+        return (preRight - preLeft)
