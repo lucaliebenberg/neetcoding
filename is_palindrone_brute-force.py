2f058aec6c9f42d693d8a9197dac25f833883d49
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-11-20 00:42:45 +0200

    chore: is palindrome - brute force

---
+++ is_palindrone_brute-force.py
@@ -0,0 +1,11 @@
+# Input: s = "Was it a car or a cat I saw?"
+
+# Output: true
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ''
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]
