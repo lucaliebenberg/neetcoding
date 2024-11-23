b5144c1cf2c2ace5aa104e0eeae8688c46a6e407
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-11-23 19:06:25 +0200

    chore: daily temperatures - stack

---
+++ dailyTemperatures.py
@@ -0,0 +1,19 @@
+# Input: temperatures = [30,38,30,36,35,40,28]
+
+# Output: [1,4,1,2,1,0,0]
+
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        res = [0] * len(temperatures)
+        stack = []
+
+        for i,t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = (i - stackInd)
+            stack.append([t,i])
+        return res
