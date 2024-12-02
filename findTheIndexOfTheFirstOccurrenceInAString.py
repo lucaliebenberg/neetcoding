f235a9899e1ab8b1f4a8d706b79056597df593a7
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-12-02 20:39:58 +0200

    chore: find the index of first occurence in a string

---
+++ findTheIndexOfTheFirstOccurrenceInAString.py
@@ -0,0 +1,25 @@
+# Example 1
+# Input: haystack = "sadbutsad", needle = "sad"
+# Output: 0
+# Explanation: "sad" occurs at index 0 and 6.
+# The first occurrence is at index 0, so we return 0.
+
+# Example 2
+# Input: haystack = "leetcode", needle = "leeto"
+# Output: -1
+# Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle == "":
+            return 0
+
+        for i in range(len(haystack) + 1 - len(needle)):
+            if haystack[i:i + len(needle)] == needle:
+                return i
+        return -1 
